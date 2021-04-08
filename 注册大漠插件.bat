5ca58ccabab0010b882817083eb388ffe71ec6ad
JhonarraonCSDN <48936526+JhonarraonCSDN@users.noreply.github.com>
2021-04-08 11:08:39 +0800

    Update 注册大漠插件.bat

--- 注册大漠插件.bat
+++ 注册大漠插件.bat
@@ -1,1 +1,7 @@
+@ echo off
+%1 %2
+ver|find "5.">nul&&goto :Admin
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof
+:Admin
+cd /d %~dp0
 regsvr32 dm.dll
